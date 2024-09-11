5b68d7b000af8f5ec3c46969ff1554d468996a22
Noah <nbaldwin3@my.bcit.ca>
2024-09-11 11:09:01 -0700

    Implemented details page grabbing information based on ID

---
+++ Pages/CategoryDetails.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NorthwindHCI.NW;
+
+namespace NorthwindHCI.Pages
+{
+    
+    public class CategoryDetailsModel : PageModel
+    {
+        private readonly NorthwindContext _context;
+
+        public CategoryDetailsModel() {
+            _context = new();
+        }
+
+        public void OnGet(int id)
+        {
+            var categories = _context.Categories
+            .OrderBy(c => c.CategoryId)
+            .Select(c => new Category {
+                CategoryName = c.CategoryName,
+                CategoryId = c.CategoryId,
+                Description = c.Description
+            });
+            ViewData["Category"] = categories.ToList()[id - 1];
+        }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NorthwindHCI.NW;
@@ -6,23 +7,24 @@ namespace NorthwindHCI.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
     private readonly NorthwindContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel()
     {
-        _logger = logger;
         _context = new();
     }
 
     public void OnGet()
     {
         var categories = _context.Categories
+            .OrderBy(c => c.CategoryId)
             .Select(c => new Category {
-                CategoryName = c.CategoryName
+                CategoryName = c.CategoryName,
+                CategoryId = c.CategoryId
             });
-        _logger.LogWarning(categories.Count().ToString());
         ViewData["Categories"] = categories.ToList();
+        foreach (Category item in categories) {
+            Console.WriteLine($"{item.CategoryId}\t{item.CategoryName}");
+        };
     }
 }
