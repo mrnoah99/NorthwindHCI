9670991cd17f5805cff035eb2b9d6e96d72813f8
Appl3Kitti3 <tdumamag2@my.bcit.ca>
2024-09-16 18:18:50 -0700

    Commented InsertCategory

--- Pages/InsertCategory.cshtml.cs
+++ Pages/InsertCategory.cshtml.cs
@@ -6,37 +6,44 @@ using NorthwindHCI.NW;
 namespace NorthwindHCI.Pages
 {
 
+    /// <summary>
+    /// The backend / code side for the InsertCategory page
+    /// Author: Teddy Dumam-Ag A01329707
+    /// Date: 2024-09-16
+    /// </summary>
     public class InsertCategoryModel : PageModel
     {
+        // Get the database context from the dependency injection container
         private readonly NorthwindContext _context;
 
-        public int MyProperty { get; set; }
+        // Bind properties for the form fields
+        [BindProperty]
+        public string Category_Name { get; set; } // Bind the Category_Name property to the form field with the same name
 
         [BindProperty]
-        public string Category_Name { get; set; }
+        public string CategoryDescription { get; set; } // Bind the CategoryDescription property to the form field with the same name
 
-        [BindProperty]
-        public string CategoryDescription { get; set; }
-
+        // Initalize the database context in the constructor
         public InsertCategoryModel()
         {
             _context = new();
         }
+
         public void OnGet()
         {
         }
 
+        // Handle the form post submission
         public void OnPost()
         {
-            Console.WriteLine($"Category Name: {Category_Name} and Description: {CategoryDescription}");
-            var category = new Category
+            var category = new Category // Create a new category object
             {
-                CategoryName = Category_Name,
-                Description = CategoryDescription
+                CategoryName = Category_Name, // with the CategoryName property set to the value of the Category_Name property
+                Description = CategoryDescription // and the Description property set to the value of the CategoryDescription property
             };
-            _context.Categories.Add(category);
-            _context.SaveChanges();
-            ViewData["Message"] = $"Category {Category_Name} with {CategoryDescription} added successfully!";
+            _context.Categories.Add(category); // then add to database
+            _context.SaveChanges(); // save
+            ViewData["Message"] = $"Category {Category_Name} with {CategoryDescription} added successfully!"; // move to a variable
         }
 
     }
